12518ee4eb71b6fa0cb510043a654a039b3d4fd0
Geoff <blotter@ldschurch.org>
2016-03-25 13:38:34 -0600

    Made local extension here to make SQLite.swift build

---
+++ SQLiteCipher/Foundation+extension.swift
@@ -0,0 +1,15 @@
+//
+//  Foundation+extension.swift
+//  SQLiteCipher
+//
+//  Created by Geoffrey Blotter on 3/25/16.
+//  Copyright © 2016 Stephen Celis. All rights reserved.
+//
+
+import Foundation
+
+extension NSData {
+	
+	public typealias Datatype = Blob.Datatype
+	
+}
